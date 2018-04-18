be118f108f2d907a25f64dcad8d5d3af9ac67e37
floxdeveloper <fritz66@alpenjodel.de>
2018-04-18 16:36:16 +0200

    First steps for Grundriss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { GridsterModule } from 'angular-gridster2';
+import { MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GridsterModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
