5a1b199fc67bf6d32685252aa7bb1a971deae9e9
floxdeveloper <fritz66@alpenjodel.de>
2018-04-19 10:17:51 +0200

    Fixed merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,38 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { GridsterModule } from 'angular-gridster2';
-<<<<<<< HEAD
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { NavbarComponent } from './navbar/navbar.component';
-=======
-import { MatToolbarModule } from '@angular/material';
-
-import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
->>>>>>> 9852ef197375f894510266ca992d668b2d1f691e
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     MainComponent,
     NavbarComponent
-=======
-    MainComponent
->>>>>>> 9852ef197375f894510266ca992d668b2d1f691e
   ],
   imports: [
     BrowserModule,
     GridsterModule,
-<<<<<<< HEAD
     NgbModule.forRoot()
-=======
-    MatToolbarModule
->>>>>>> 9852ef197375f894510266ca992d668b2d1f691e
   ],
   providers: [],
   bootstrap: [AppComponent]
