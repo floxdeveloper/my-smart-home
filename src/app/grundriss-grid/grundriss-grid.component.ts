e5d006416c02a8167082679b3215eeaf8fda695a
floxdeveloper <fritz66@alpenjodel.de>
2018-05-14 12:58:58 +0200

    Added initialisation of two dimensional things array to Grundriss

--- src/app/grundriss-grid/grundriss-grid.component.ts
+++ src/app/grundriss-grid/grundriss-grid.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Thing } from '../entity/thing';
 import { Action } from '../entity/action';
 import { State } from '../entity/state';
+import { PostParameter } from '../entity/post-parameter';
 
 @Component({
   selector: 'app-grundriss-grid',
@@ -10,16 +11,16 @@ import { State } from '../entity/state';
   styleUrls: ['./grundriss-grid.component.css']
 })
 export class GrundrissGridComponent implements OnInit {
-  panels: PanelComponent[][];
+  things: Thing[][];
 
   constructor() {
-    this.panels = Array<Array<PanelComponent>>();
+    this.things = Array<Array<Thing>>();
     for (let i = 0; i < 8; i++) {
-      this.panels[i] = [];
+      this.things[i] = [];
     }
-    const bettlicht = new Thing('Bettlicht', '../assets/images/bettlicht.png', new Action('Schalter', 'http://192.168.178.30:8080/rest/items/homematic_HM_ES_PMSw1_Pl_DN_R1_9d365d5c_NEQ0706282_1_STATE'), [], new State('Licht', 'http://192.168.178.30:8080/rest/items/homematic_HM_ES_PMSw1_Pl_DN_R1_9d365d5c_NEQ0706282_1_STATE', '0'), []);
-    this.panels[5][1] = new PanelComponent(bettlicht);
-    console.log(this.panels[5][1]);
+    const bettlicht = new Thing('Bettlicht', '../assets/images/bettlicht.png', new Action('Schalter', new PostParameter('rest/items/homematic_HM_ES_PMSw1_Pl_DN_R1_9d365d5c_NEQ0706282_1_STATE', 'ON', null)), [], new State('Licht', 'http://192.168.178.30:8080/rest/items/homematic_HM_ES_PMSw1_Pl_DN_R1_9d365d5c_NEQ0706282_1_STATE', '0'), []);
+    this.things[5][1] = bettlicht;
+    console.log(this.things[5][1]);
   }
 
   ngOnInit() {
